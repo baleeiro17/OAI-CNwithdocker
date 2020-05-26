83010e57675bb9341edbf493d4404c6ecfe69432
lucas baleeiro dominato <lucasdominato2@gmail.com>
2020-05-26 00:39:38 -0300

    solved communication problem between hss and mysql

--- hss/script.sh
+++ hss/script.sh
@@ -10,4 +10,7 @@ echo " --> checking for certificates..."
 ./oai-cn/scripts/check_hss_s6a_certificate /usr/local/etc/oai/freeDiameter $(hostname)
 
 # import database.
-mysql --host "mysqldb" -u "root" -p "linux" "oai_db" < /oai_cn/src/oai_hss/db/oai_db.sql && echo "[OK] Database Updated" || echo "[FAIL] Database Non Updated"
+mysql --host "mysqldb" -u "root" -p"linux" "oai_db" < /oai-cn/src/oai_hss/db/oai_db.sql && echo "[OK] Database Updated" || echo "[FAIL] Database Non Updated"
+
+# running hss
+./oai-cn/scripts/run_hss 
